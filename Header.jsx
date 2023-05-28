e0bd77bcf5ffe8b120874b1485df077e883007aa
Borya <130313383+borya92@users.noreply.github.com>
2023-05-29 01:47:06 +0400

    Create Header.jsx

---
+++ Header.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+class MyHeader extends React.Component {
+  render() {
+    const mystyle = {
+      color: "white",
+      backgroundColor: "DodgerBlue",
+      padding: "10px",
+      fontFamily: "Arial"
+    };
+    return (
+      <div>
+      <h1 style={mystyle}>Hello Style!</h1>
+      <p>Add a little style!</p>
+      </div>
+    );
+  }
+}
+
+export default MyHeader;
