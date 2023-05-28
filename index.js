6ab546592a3ab7b799c59870dcb54cfbe440a294
Borya <130313383+borya92@users.noreply.github.com>
2023-05-29 01:48:36 +0400

    Update index.js

--- index.js
+++ index.js
@@ -1,20 +1,11 @@
-import React from "react";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import Header from './Header';
 
-class MyHeader extends React.Component {
-  render() {
-    const mystyle = {
-      color: "white",
-      backgroundColor: "DodgerBlue",
-      padding: "10px",
-      fontFamily: "Arial"
-    };
-    return (
-      <div>
-      <h1 style={mystyle}>Hello Style!</h1>
-      <p>Add a little style!</p>
-      </div>
-    );
-  }
-}
+const root = ReactDOM.createRoot(document.getElementById('root'));
 
-export default MyHeader;
+root.render(
+  <React.StrictMode>
+    <Header />
+  </React.StrictMode>
+);
